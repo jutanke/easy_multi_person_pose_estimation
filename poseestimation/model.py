2903d2fc579afecd2dbabc7a4151ba9687a0835f
julian.tanke <julian.tanke@fit.fraunhofer.de>
2018-03-19 17:59:41 +0100

    export points

--- poseestimation/model.py
+++ poseestimation/model.py
@@ -12,6 +12,7 @@ from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.normalization import BatchNormalization
 from keras.layers.merge import Concatenate
+import math
 
 # -----------------------------------
 # neural network part
@@ -206,17 +207,18 @@ class PoseEstimator:
         """
         if len(X.shape) == 3:  # single image: make it big
             X = np.expand_dims(X, 0)
-        n = X.shape[0]
+        n, h, w, _ = X.shape
         thre1 = self.peak_threshold
 
         # get the heatmaps and pafs for all scales for each
         # entry in X
         heatmaps, pafs = self.predict_pafs_and_heatmaps(X)
 
-        all_peaks = []
-        peak_counter = 0
+        POSITIONS = []
 
         for i in range(n):
+            all_peaks = []
+            peak_counter = 0
             for part in range(19-1):
                 hm = heatmaps[i][:,:,part]
                 blur = gaussian_filter(hm, sigma=3)  #TODO use cv2 here..
@@ -245,10 +247,150 @@ class PoseEstimator:
                 all_peaks.append(peaks_with_score_and_id)
                 peak_counter += len(peaks)
 
-        return all_peaks
+            # -----------------------
+            mid_num = 10
+            paf_avg = np.squeeze(pafs)
+            thre2 = 0.05
+            special_k = []
+            connection_all = []
 
+            limbSeq = [[2, 3], [2, 6], [3, 4], [4, 5], [6, 7], [7, 8], [2, 9], [9, 10], \
+                       [10, 11], [2, 12], [12, 13], [13, 14], [2, 1], [1, 15], [15, 17], \
+                       [1, 16], [16, 18], [3, 17], [6, 18]]
 
-    def predict_pafs_and_heatmaps(self, X, Pairs):
+            mapIdx = [[31, 32], [39, 40], [33, 34], [35, 36], [41, 42], [43, 44], [19, 20], [21, 22], \
+                      [23, 24], [25, 26], [27, 28], [29, 30], [47, 48], [49, 50], [53, 54], [51, 52], \
+                      [55, 56], [37, 38], [45, 46]]
+
+            for k in range(len(mapIdx)):
+                score_mid = paf_avg[:, :, [x - 19 for x in mapIdx[k]]]
+                candA = all_peaks[limbSeq[k][0] - 1]
+                candB = all_peaks[limbSeq[k][1] - 1]
+                #indexA, indexB = limbSeq[k]
+                nA = len(candA)
+                nB = len(candB)
+                if nA != 0 and nB != 0:
+                    connection_candidate = []
+                    for i in range(nA):
+                        for j in range(nB):
+                            vec = np.subtract(candB[j][:2], candA[i][:2])
+                            norm = math.sqrt(vec[0] * vec[0] + vec[1] * vec[1])
+                            if norm == 0:
+                                continue
+                            vec = np.divide(vec, norm)
+
+                            startend = list(zip(np.linspace(candA[i][0], candB[j][0], num=mid_num), \
+                                                np.linspace(candA[i][1], candB[j][1], num=mid_num)))
+
+                            vec_x = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 0] \
+                                              for I in range(len(startend))])
+                            vec_y = np.array([score_mid[int(round(startend[I][1])), int(round(startend[I][0])), 1] \
+                                              for I in range(len(startend))])
+
+                            score_midpts = np.multiply(vec_x, vec[0]) + np.multiply(vec_y, vec[1])
+                            score_with_dist_prior = sum(score_midpts) / len(score_midpts) + min(0.5 * h / norm - 1, 0)
+                            criterion1 = len(np.nonzero(score_midpts > thre2)[0]) > 0.8 * len(score_midpts)
+                            criterion2 = score_with_dist_prior > 0
+
+                            if criterion1 and criterion2:
+                                connection_candidate.append(
+                                    [i, j, score_with_dist_prior, score_with_dist_prior + candA[i][2] + candB[j][2]])
+
+                    connection_candidate = sorted(connection_candidate, key=lambda x: x[2], reverse=True)
+                    connection = np.zeros((0, 5))
+                    for c in range(len(connection_candidate)):
+                        i, j, s = connection_candidate[c][0:3]
+                        if (i not in connection[:, 3] and j not in connection[:, 4]):
+                            connection = np.vstack([connection, [candA[i][3], candB[j][3], s, i, j]])
+                            if (len(connection) >= min(nA, nB)):
+                                break
+
+                    connection_all.append(connection)
+                else:
+                    special_k.append(k)
+                    connection_all.append([])
+
+            # last number in each row is the total parts number of that person
+            # the second last number in each row is the score of the overall configuration
+            subset = -1 * np.ones((0, 20))
+            candidate = np.array([item for sublist in all_peaks for item in sublist])
+
+            for k in range(len(mapIdx)):
+                if k not in special_k:
+                    partAs = connection_all[k][:, 0]
+                    partBs = connection_all[k][:, 1]
+                    indexA, indexB = np.array(limbSeq[k]) - 1
+
+                    for i in range(len(connection_all[k])):  # = 1:size(temp,1)
+                        found = 0
+                        subset_idx = [-1, -1]
+                        for j in range(len(subset)):  # 1:size(subset,1):
+                            if subset[j][indexA] == partAs[i] or subset[j][indexB] == partBs[i]:
+                                subset_idx[found] = j
+                                found += 1
+
+                        if found == 1:
+                            j = subset_idx[0]
+                            if (subset[j][indexB] != partBs[i]):
+                                subset[j][indexB] = partBs[i]
+                                subset[j][-1] += 1
+                                subset[j][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
+                        elif found == 2:  # if found 2 and disjoint, merge them
+                            j1, j2 = subset_idx
+                            print("found = 2")
+                            membership = ((subset[j1] >= 0).astype(int) + (subset[j2] >= 0).astype(int))[:-2]
+                            if len(np.nonzero(membership == 2)[0]) == 0:  # merge
+                                subset[j1][:-2] += (subset[j2][:-2] + 1)
+                                subset[j1][-2:] += subset[j2][-2:]
+                                subset[j1][-2] += connection_all[k][i][2]
+                                subset = np.delete(subset, j2, 0)
+                            else:  # as like found == 1
+                                subset[j1][indexB] = partBs[i]
+                                subset[j1][-1] += 1
+                                subset[j1][-2] += candidate[partBs[i].astype(int), 2] + connection_all[k][i][2]
+
+                        # if find no partA in the subset, create a new subset
+                        elif not found and k < 17:
+                            row = -1 * np.ones(20)
+                            row[indexA] = partAs[i]
+                            row[indexB] = partBs[i]
+                            row[-1] = 2
+                            row[-2] = sum(candidate[connection_all[k][i, :2].astype(int), 2]) + connection_all[k][i][2]
+                            subset = np.vstack([subset, row])
+
+            # delete some rows of subset which has few parts occur
+            deleteIdx = [];
+            for i in range(len(subset)):
+                if subset[i][-1] < 4 or subset[i][-2] / subset[i][-1] < 0.4:
+                    deleteIdx.append(i)
+            subset = np.delete(subset, deleteIdx, axis=0)
+
+            # POSITIONS -> (n, #persons, 17)
+            nbr_persons = len(subset)
+            cur_positions = []
+            for _ in range(nbr_persons):
+                cur_positions.append([None] * 18)
+
+            for j in range(17):
+                for k in range(nbr_persons):
+                    index = subset[k][np.array(limbSeq[j]) - 1]
+                    if -1 in index:
+                        continue
+
+                    # joint position:
+                    a, b = np.array(limbSeq[j]) - 1
+                    _Y = candidate[index.astype(int), 0]
+                    _X = candidate[index.astype(int), 1]
+                    cur_positions[k][a] = (_X[0], _Y[0])
+                    cur_positions[k][b] = (_X[1], _Y[1])
+
+            POSITIONS.append(np.array(cur_positions))
+
+        # ------------------------
+
+        return POSITIONS
+
+    def predict_pafs_and_heatmaps(self, X):
         """
 
         :param X:
@@ -280,11 +422,6 @@ class PoseEstimator:
                 X_resized[i] = I_res
 
             pafs, heatmaps = self.model.predict(X_resized)
-
-            Pairs.append((
-                np.squeeze(np.swapaxes(X_resized, 1, 2)),
-                heatmaps
-            ))
 
             for i in range(n):
                 hm = cv2.resize(heatmaps[i], (0,0), fx=stride, fy=stride, interpolation=cv2.INTER_CUBIC)
