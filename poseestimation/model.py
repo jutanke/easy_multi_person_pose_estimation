d698db5f74ba504d2e8a3e94c5ed769e120ea34e
julian.tanke <julian.tanke@fit.fraunhofer.de>
2018-03-19 16:02:00 +0100

    fix orientation bug

--- poseestimation/model.py
+++ poseestimation/model.py
@@ -5,6 +5,109 @@ import shutil
 from keras.models import load_model
 from os.path import join, isfile
 from scipy.ndimage.filters import gaussian_filter
+from keras.models import Sequential
+from keras.models import Model
+from keras.layers import Input, Dense, Activation, Lambda
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.merge import Concatenate
+
+# -----------------------------------
+# neural network part
+# -----------------------------------
+
+def relu(x):
+    return Activation('relu')(x)
+
+
+def conv(x, nf, ks, name):
+    x1 = Conv2D(nf, (ks, ks), padding='same', name=name)(x)
+    return x1
+
+
+def pooling(x, ks, st, name):
+    x = MaxPooling2D((ks, ks), strides=(st, st), name=name)(x)
+    return x
+
+
+def vgg_block(x):
+    # Block 1
+    x = conv(x, 64, 3, "conv1_1")
+    x = relu(x)
+    x = conv(x, 64, 3, "conv1_2")
+    x = relu(x)
+    x = pooling(x, 2, 2, "pool1_1")
+
+    # Block 2
+    x = conv(x, 128, 3, "conv2_1")
+    x = relu(x)
+    x = conv(x, 128, 3, "conv2_2")
+    x = relu(x)
+    x = pooling(x, 2, 2, "pool2_1")
+
+    # Block 3
+    x = conv(x, 256, 3, "conv3_1")
+    x = relu(x)
+    x = conv(x, 256, 3, "conv3_2")
+    x = relu(x)
+    x = conv(x, 256, 3, "conv3_3")
+    x = relu(x)
+    x = conv(x, 256, 3, "conv3_4")
+    x = relu(x)
+    x = pooling(x, 2, 2, "pool3_1")
+
+    # Block 4
+    x = conv(x, 512, 3, "conv4_1")
+    x = relu(x)
+    x = conv(x, 512, 3, "conv4_2")
+    x = relu(x)
+
+    # Additional non vgg layers
+    x = conv(x, 256, 3, "conv4_3_CPM")
+    x = relu(x)
+    x = conv(x, 128, 3, "conv4_4_CPM")
+    x = relu(x)
+
+    return x
+
+
+def stage1_block(x, num_p, branch):
+    # Block 1
+    x = conv(x, 128, 3, "conv5_1_CPM_L%d" % branch)
+    x = relu(x)
+    x = conv(x, 128, 3, "conv5_2_CPM_L%d" % branch)
+    x = relu(x)
+    x = conv(x, 128, 3, "conv5_3_CPM_L%d" % branch)
+    x = relu(x)
+    x = conv(x, 512, 1, "conv5_4_CPM_L%d" % branch)
+    x = relu(x)
+    x = conv(x, num_p, 1, "conv5_5_CPM_L%d" % branch)
+
+    return x
+
+
+def stageT_block(x, num_p, stage, branch):
+    # Block 1
+    x = conv(x, 128, 7, "Mconv1_stage%d_L%d" % (stage, branch))
+    x = relu(x)
+    x = conv(x, 128, 7, "Mconv2_stage%d_L%d" % (stage, branch))
+    x = relu(x)
+    x = conv(x, 128, 7, "Mconv3_stage%d_L%d" % (stage, branch))
+    x = relu(x)
+    x = conv(x, 128, 7, "Mconv4_stage%d_L%d" % (stage, branch))
+    x = relu(x)
+    x = conv(x, 128, 7, "Mconv5_stage%d_L%d" % (stage, branch))
+    x = relu(x)
+    x = conv(x, 128, 1, "Mconv6_stage%d_L%d" % (stage, branch))
+    x = relu(x)
+    x = conv(x, num_p, 1, "Mconv7_stage%d_L%d" % (stage, branch))
+
+    return x
+
+# -----------------------------------
+# neural network part
+# -----------------------------------
 
 
 def padRightDownCorner(img, stride, padValue):
@@ -53,13 +156,47 @@ class PoseEstimator:
         self.scale_search = scale_search
         self.peak_threshold = peak_threshold
 
-        modelf = join(model_dir, 'poseestimation.h5')
+        modelf = join(model_dir, 'model_weights.h5')
         if not isfile(modelf):
-            url = 'http://188.138.127.15:81/models/poseestimation.h5'
+            url = 'http://188.138.127.15:81/models/model_weights.h5'
             with urllib.request.urlopen(url) as res, open(modelf, 'wb') as f:
                 shutil.copyfileobj(res, f)
 
-        self.model = load_model(modelf)
+        input_shape = (None, None, 3)
+
+        img_input = Input(shape=input_shape)
+
+        stages = 6
+        np_branch1 = 38
+        np_branch2 = 19
+
+        img_normalized = Lambda(lambda x: x / 256 - 0.5)(img_input)  # [-0.5, 0.5]
+
+        # VGG
+        stage0_out = vgg_block(img_normalized)
+
+        # stage 1
+        stage1_branch1_out = stage1_block(stage0_out, np_branch1, 1)
+        stage1_branch2_out = stage1_block(stage0_out, np_branch2, 2)
+        x = Concatenate()([stage1_branch1_out, stage1_branch2_out, stage0_out])
+
+        # stage t >= 2
+        for sn in range(2, stages + 1):
+            stageT_branch1_out = stageT_block(x, np_branch1, sn, 1)
+            stageT_branch2_out = stageT_block(x, np_branch2, sn, 2)
+            if (sn < stages):
+                x = Concatenate()([stageT_branch1_out, stageT_branch2_out, stage0_out])
+
+        model = Model(img_input, [stageT_branch1_out, stageT_branch2_out])
+        model.load_weights(modelf)
+        self.model = model
+
+        # modelf = join(model_dir, 'poseestimation.h5')
+        # if not isfile(modelf):
+        #     url = 'http://188.138.127.15:81/models/poseestimation.h5'
+        #     with urllib.request.urlopen(url) as res, open(modelf, 'wb') as f:
+        #         shutil.copyfileobj(res, f)
+        # self.model = load_model(modelf)
 
     def predict(self, X):
         """
@@ -111,7 +248,7 @@ class PoseEstimator:
         return all_peaks
 
 
-    def predict_pafs_and_heatmaps(self, X):
+    def predict_pafs_and_heatmaps(self, X, Pairs):
         """
 
         :param X:
@@ -129,8 +266,8 @@ class PoseEstimator:
         multiplier = [x * boxsize / h for x in scale_search]
 
         nbr_scales = len(scale_search)
-        heatmaps_over_scales = np.zeros((n, nbr_scales, h, w, 19))
-        pafs_over_scales = np.zeros((n, nbr_scales, h, w, 38))
+        Heatmaps = np.zeros((n, h, w, 19))
+        Pafs = np.zeros((n, h, w, 38))
 
         for j, scale in enumerate(multiplier):
             w_res = int(w * scale); h_res = int(h * scale)
@@ -142,24 +279,21 @@ class PoseEstimator:
                 I_res, pad = padRightDownCorner(I_res, stride, padValue)
                 X_resized[i] = I_res
 
-            # the required shape is (n,w,h,3) but atm it is (n,h,w,3)
-            X_resized = np.swapaxes(X_resized, 1, 2)
-
             pafs, heatmaps = self.model.predict(X_resized)
 
-            # 'restore' the old order -> h,w
-            heatmaps = np.swapaxes(heatmaps, 1, 2)
-            pafs = np.swapaxes(pafs, 1, 2)
+            Pairs.append((
+                np.squeeze(np.swapaxes(X_resized, 1, 2)),
+                heatmaps
+            ))
 
             for i in range(n):
                 hm = cv2.resize(heatmaps[i], (0,0), fx=stride, fy=stride, interpolation=cv2.INTER_CUBIC)
-                hm = cv2.resize(hm[0:h,0:w], (w,h), interpolation=cv2.INTER_CUBIC)
-                heatmaps_over_scales[i, j] = hm
+                hm = cv2.resize(hm[0:h_res,0:w_res], (w,h), interpolation=cv2.INTER_CUBIC)
+                Heatmaps[i] = Heatmaps[i] + hm / nbr_scales
 
                 paf = cv2.resize(pafs[i], (0,0), fx=stride, fy=stride, interpolation=cv2.INTER_CUBIC)
-                paf = cv2.resize(paf[0:h,0:w], (w, h), interpolation=cv2.INTER_CUBIC)
-                pafs_over_scales[i,j] = paf
+                paf = cv2.resize(paf[0:h_res,0:w_res], (w, h), interpolation=cv2.INTER_CUBIC)
+                Pafs[i] = Pafs[i] + paf / nbr_scales
 
 
-        return np.mean(heatmaps_over_scales, axis=1), \
-               np.mean(pafs_over_scales, axis=1)
+        return Heatmaps, Pafs
